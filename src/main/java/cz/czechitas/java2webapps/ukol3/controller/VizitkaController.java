2f2ba0d6cfc7114ee4aa44bbba7d804363e219d6
Frolíková Karolina <frolikova.karolina@gmail.com>
2024-05-11 08:56:03 +0200

    hotovo

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -3,6 +3,7 @@ package cz.czechitas.java2webapps.ukol3.controller;
 import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.awt.*;
@@ -14,14 +15,12 @@ import java.util.List;
  */
 @Controller
 public class VizitkaController {
-
     private final List<Vizitka> seznamVizitek = new ArrayList<>();
 
     public VizitkaController() {
         seznamVizitek.add(new Vizitka("Petr Novák", "Seznam", "Nováková 45", "Brno, 12345", "petr.novak@seznam.cz", "+420 123 456 789", "wwww.seznam.cz"));
-        seznamVizitek.add(new Vizitka("Marie Nováková", "Google", "Nováková 45", "Brno, 12345", "marie.novak@seznam.cz", "+420 123 456 987", "wwww.google.com"));
-        seznamVizitek.add(new Vizitka("Alice Samková", "Žádná firma", "Ostravská 789", "Ostrava, 70030", null, null, null));
-        seznamVizitek.add(new Vizitka("Martin Blažený", "Testovací s.r.o.", "Liberecká 101", "Liberec, 46001", "martin.blazeny@example.com", null, null));
+        seznamVizitek.add(new Vizitka("Alice Samková", "Coca‑Cola", "Českobrodská 1329", "Praha 9 – Kyje, 198 21", null, "+420 777 666 555", null));
+        seznamVizitek.add(new Vizitka("Martin Blažený", "Testovací s.r.o.", "Okrajová 517/8", "Těrlicko, 73542", "martin.blazeny@example.com", null, "www.testovaci.cz"));
     }
 
     @GetMapping("/")
@@ -31,8 +30,10 @@ public class VizitkaController {
         return result;
     }
 
-    @GetMapping("/detail")
-    public String detail() {
-        return "detail";
+    @GetMapping("/detail/{id}")
+    public ModelAndView detail(@PathVariable int id) {
+        ModelAndView model = new ModelAndView("detail");
+        model.addObject("vizitky", seznamVizitek.get(id));
+        return model;
     }
 }
